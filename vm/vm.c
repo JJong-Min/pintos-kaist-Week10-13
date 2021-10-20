df71171acfd97c6209419f33fc1ecc6734b652a6
JJong-Min <sambujakkk@naver.com>
2021-10-20 22:44:31 +0900

    vm_claim_page 함수 구현

--- vm/vm.c
+++ vm/vm.c
@@ -222,10 +222,9 @@ vm_dealloc_page (struct page *page) {
 
 /* Claim the page that allocate on VA. */
 bool
-vm_claim_page (void *va UNUSED) {
-	struct page *page = NULL;
-	/* TODO: Fill this function */
-
+vm_claim_page (void *va) {
+	struct page *page = spt_find_page (&thread_current () ->spt, va);
+	if (page == NULL) return false;
 	return vm_do_claim_page (page);
 }
 
