35879ef7995002438fa62574d6b7ab0a6a4fd25d
JJong-Min <sambujakkk@naver.com>
2021-10-21 22:16:09 +0900

    anonymous page 중간 구현

--- include/vm/file.h
+++ include/vm/file.h
@@ -12,6 +12,12 @@ struct file_page {
 	off_t ofs;
 };
 
+struct mmap_info{
+	struct file* file;
+	off_t offset;
+	size_t read_bytes;
+};
+
 void vm_file_init (void);
 bool file_backed_initializer (struct page *page, enum vm_type type, void *kva);
 void *do_mmap(void *addr, size_t length, int writable,
