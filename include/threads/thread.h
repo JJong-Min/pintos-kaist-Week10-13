bfe1d39b6b1bba77188dafa17f85612700befa23
JJong-Min <sambujakkk@naver.com>
2021-10-22 00:01:39 +0900

    spt, saved_sp 멤버 추가

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -142,10 +142,10 @@ struct thread {
 #ifdef VM
 	/* Table for whole virtual memory owned by thread. */
 	struct supplemental_page_table spt;
+	uintptr_t saved_sp;
 	
 	/* Saving rsp into struct thread on the initial transition
 	 * from user to kernel mode. */
-	uintptr_t saved_sp;
 #endif
 
 	/* Owned by thread.c. */
