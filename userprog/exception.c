981821a2a1fd5529859620eb72f0e054151bd85f
JJong-Min <sambujakkk@naver.com>
2021-10-23 00:07:31 +0900

    kill 함수 수정(anon page 구현완료)

--- userprog/exception.c
+++ userprog/exception.c
@@ -85,9 +85,9 @@ kill (struct intr_frame *f) {
 		case SEL_UCSEG:
 			/* User's code segment, so it's a user exception, as we
 			   expected.  Kill the user process.  */
-			printf ("%s: dying due to interrupt %#04llx (%s).\n",
-					thread_name (), f->vec_no, intr_name (f->vec_no));
-			intr_dump_frame (f);
+			//printf ("%s: dying due to interrupt %#04llx (%s).\n",
+					//thread_name (), f->vec_no, intr_name (f->vec_no));
+			//intr_dump_frame (f);
 			thread_exit ();
 
 		case SEL_KCSEG:
@@ -149,16 +149,16 @@ page_fault (struct intr_frame *f) {
 	if (vm_try_handle_fault (f, fault_addr, user, write, not_present))
 		return;
 #endif
-	_exit(-1);
+	// _exit(-1);
 	/* Count page faults. */
 	page_fault_cnt++;
 
 	/* If the fault is true fault, show info and exit. */
-	printf ("Page fault at %p: %s error %s page in %s context.\n",
-			fault_addr,
-			not_present ? "not present" : "rights violation",
-			write ? "writing" : "reading",
-			user ? "user" : "kernel");
+	// printf ("Page fault at %p: %s error %s page in %s context.\n",
+	// 		fault_addr,
+	// 		not_present ? "not present" : "rights violation",
+	// 		write ? "writing" : "reading",
+	// 		user ? "user" : "kernel");
 	kill (f);
 }
 
