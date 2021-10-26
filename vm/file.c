17e4a2fa68a5ffab721de8e9519d1a9f6517de35
JJong-Min <sambujakkk@naver.com>
2021-10-26 21:03:02 +0900

    do_mmap()함수 구현

--- vm/file.c
+++ vm/file.c
@@ -39,12 +39,12 @@ vm_file_init (void) {
 bool
 file_backed_initializer (struct page *page, enum vm_type type, void *kva) {
 	/* Set up the handler */
-// 	struct file *file = ((struct mmap_info *)page->uninit.aux)->file;
-// 	page->operations = &file_ops;
+	struct file* file = ((struct mmap_info*)page ->uninit.aux)->file;
+	page->operations = &file_ops;
 
-// 	struct file_page *file_page = &page->file;
-// 	file_page -> file = file;
-// 	return true;
+	struct file_page *file_page = &page->file;
+	file_page -> file = file;
+	return true;
 }
 
 
@@ -63,13 +63,57 @@ file_backed_swap_out (struct page *page) {
 /* Destory the file backed page. PAGE will be freed by the caller. */
 static void
 file_backed_destroy (struct page *page) {
-	struct file_page *file_page UNUSED = &page->file;
+	// TODO: On mmap_exit sometimes empty file content
+	struct file_page *file_page = &page->file;
+	//if dirty, write back to file
+	if (pml4_is_dirty (thread_current() -> pml4, page -> va)){
+		file_seek (file_page->file, file_page->ofs);
+		file_write (file_page->file, page->va, file_page->size);
+	}
+	file_close (file_page->file);
+
+	if (page->frame != NULL) {
+		list_remove (&page->frame->elem);
+		free (page->frame);
+	}
+}
+
+//Use to lazy load mmap
+static bool
+lazy_load_file (struct page* page, void* aux){
+	struct mmap_info* mi = (struct mmap_info*) aux;
+	file_seek (mi->file, mi->offset);
+	page -> file.size = file_read (mi->file, page->va, mi->read_bytes);
+	page -> file.ofs = mi->offset;
+	if (page->file.size != PGSIZE){
+		memset (page->va + page->file.size, 0, PGSIZE - page->file.size);
+	}
+	pml4_set_dirty (thread_current()->pml4, page->va, false);
+	free(mi);
+	return true;
 }
 
 /* Do the mmap */
 void *
 do_mmap (void *addr, size_t length, int writable,
 		struct file *file, off_t offset) {
+	//assume all parameter errors are handled in syscall.c
+	off_t ofs;
+	uint64_t read_bytes;
+	for (uint64_t i = 0; i < length; i += PGSIZE){
+		struct mmap_info* mi = malloc (sizeof (struct mmap_info));
+		ofs = offset + i;
+		read_bytes = length - i >= PGSIZE ? PGSIZE : length -i;
+		mi->file = file_reopen (file);
+		mi->offset = ofs;
+		mi->read_bytes = read_bytes;
+		vm_alloc_page_with_initializer (VM_FILE, (void*) ((uint64_t) addr + i), writable, lazy_load_file, (void*) mi);
+	}
+	struct mmap_file_info* mfi = malloc (sizeof (struct mmap_file_info));
+	mfi->start = (uint64_t) addr;
+	mfi->end = (uint64_t) pg_round_down((uint64_t) addr + length -1);
+	list_push_back(&mmap_file_list, &mfi->elem);
+	return addr;
 }
 
 /* Do the munmap */
